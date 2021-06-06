d5f89a44594b506e0a60eae6fdff288e96605f00
U-MBOOK\dev <jongmyun.moon@gmail.com>
2021-06-06 11:46:28 -0700

    init script udpate

--- script/setup.sh
+++ script/setup.sh
@@ -1,29 +1,28 @@
 #!/usr/bin/env bash
 
-function add {
+function add_source {
     touch $2
-    grep -qxF "source $1" $2 || echo "source $1" >> $2 && echo "Set up $2"
+    grep -qxF "source $1" $2 && echo "$2 is already set" || echo "source $1" >> $2 && echo "Set up $2"
 }
 
 # Bash
-add ~/.myconfig/bash/bash_profile.bash ~/.bash_profile
-add ~/.myconfig/bash/bashrc.bash ~/.bashrc
+ln -sf ~/.myconfig/bash/bash_profile.bash ~/.bash_profile
+ln -sf ~/.myconfig/bash/bashrc.bash ~/.bashrc
 
 # Tmux
-add ~/.myconfig/tmux/tmux_conf.tmux ~/.tmux.conf
+ln -sf ~/.myconfig/tmux/tmux_conf.tmux ~/.tmux.conf
 
 # Fish
 mkdir -p ~/.config
 mkdir -p ~/.config/fish
 mkdir -p ~/.config/fish/functions
 
-add ~/.myconfig/fish/config.fish ~/.config/fish/config.fish
+ln -sf ~/.myconfig/fish/config.fish ~/.config/fish/config.fish
 ln -sf ~/.myconfig/fish/functions/* ~/.config/fish/functions
 
 # Vim
-add ~/.myconfig/vim/vimrc.vim ~/.vimrc
+ln -sf ~/.myconfig/vim/vimrc.vim ~/.vim/vimrc
 
 # Vifm
 mkdir -p ~/.config/vifm
-add ~/.myconfig/vifm/vifmrc.vim ~/.config/vifm/vifmrc
-
+add_source ~/.myconfig/vifm/vifmrc.vim ~/.config/vifm/vifmrc
