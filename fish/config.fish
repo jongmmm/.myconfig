37ca80062e755e68e15ffc1491aa54586a6d2046
U-MBOOK\dev <jongmyun.moon@gmail.com>
2021-06-06 11:46:09 -0700

    fish config update

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,7 @@
 . ~/.myconfig/fish/abbr.fish
 . ~/.myconfig/fish/apps.fish
+if test -f ~/.config/fish/config_local.fish
+    echo "Local config found: sourcing it"
+    . ~/.config/fish/config_local.fish
+end
 cd ~
