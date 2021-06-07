f3842bdbd44d05d47f640c014067ef31e3bdae7b
U-MBOOK\dev <jongmyun.moon@gmail.com>
2021-06-07 15:03:59 -0700

    fish cleanup

--- fish/apps.fish
+++
@@ -1,3 +0,0 @@
-if command -v pazi >/dev/null
-  status --is-interactive; and pazi init fish | source
-end
--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,4 @@
 . ~/.myconfig/fish/abbr.fish
-. ~/.myconfig/fish/apps.fish
 if test -f ~/.config/fish/config_local.fish
     . ~/.config/fish/config_local.fish
 end
