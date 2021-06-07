f8281480df8a6e7c919b92f853bd86f0ed5a20f5
U-MBOOK\dev <jongmyun.moon@gmail.com>
2021-06-06 20:42:04 -0700

    tm = tmux in fish

--- fish/abbr.fish
+++ fish/abbr.fish
@@ -3,6 +3,7 @@ abbr -a .. 'cd ..'
 abbr -a ... 'cd ../..'
 abbr -a tr tree
 abbr -a td tree -d
+abbr -a tm env SHELL=(command -v fish) tmux
 
 # git
 abbr -a gr 'cd (git rev-parse --show-toplevel)'  # Jump to the repo root
