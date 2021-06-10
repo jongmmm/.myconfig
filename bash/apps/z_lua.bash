3fd8bd5bf7978c49d219d4995b45e0414b9ef58f
U-MBOOK\dev <jongmyun.moon@gmail.com>
2021-06-09 22:35:15 -0700

    add /dev/null

--- bash/apps/z_lua.bash
+++ bash/apps/z_lua.bash
@@ -1,3 +1,5 @@
-test -d ~/local/z.lua && command -v lua && command -v fzf &> /dev/null \
+test -d ~/local/z.lua \
+    && command -v lua  &> /dev/null \
+    && command -v fzf &> /dev/null \
     && eval "$(lua ~/local/z.lua/z.lua --init bash enhanced once fzf)" \
     || eval "$(lua ~/local/z.lua/z.lua --init bash enhanced once)"
