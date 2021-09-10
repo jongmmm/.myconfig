c41f025e4f9903319471ec5f1eb9b6a4f9a148cc
U-MBOOK\dev <jongmyun.moon@gmail.com>
2021-09-09 20:31:59 -0700

    move z_lua

--- bash/apps/z_lua.bash
+++ bash/apps/z_lua.bash
@@ -1,5 +1,5 @@
-test -d ~/local/z.lua \
+test -d ~/local/bin/z.lua \
     && command -v lua  &> /dev/null \
     && command -v fzf &> /dev/null \
-    && eval "$(lua ~/local/z.lua/z.lua --init bash enhanced once fzf)" \
-    || eval "$(lua ~/local/z.lua/z.lua --init bash enhanced once)"
+    && eval "$(lua ~/local/bin/z.lua/z.lua --init bash enhanced once fzf)" \
+    || eval "$(lua ~/local/binb/z.lua/z.lua --init bash enhanced once)"
