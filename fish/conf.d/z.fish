35c9e44cb89a1effc78c1b267f6d60bedd2ce58a
U-MBOOK\dev <jongmyun.moon@gmail.com>
2021-09-14 17:33:41 -0700

    fix path

--- fish/conf.d/z.fish
+++ fish/conf.d/z.fish
@@ -1,4 +1,4 @@
-if test -d ~/local/z.lua
+if test -d ~/local/bin/z.lua
     set -gx _ZL_CD cd
-    lua ~/local/z.lua/z.lua --init fish | source
+    lua ~/local/bin/z.lua/z.lua --init fish | source
 end
