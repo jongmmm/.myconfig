c28ebd72d0b0e413a8659ca7307da2b5d77d8993
U-MBOOK\dev <jongmyun.moon@gmail.com>
2021-09-14 17:39:04 -0700

    vf .

--- fish/functions/vf.fish
+++ fish/functions/vf.fish
@@ -1,5 +1,5 @@
 function vf
-    set -l dst (command vifm --choose-dir - $argv)
+    set -l dst (command vifm . --choose-dir - $argv)
     if not test -d $dst
         echo Directory picking cancelled/failed
         return 1
