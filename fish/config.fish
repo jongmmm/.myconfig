05c3c2ec9c8fbfa4c328c0badd880b6c4c0dc3f9
Jong-Myun Moon <jmmm@mBookAir.local>
2022-01-12 13:07:31 -0800

    vi key binding

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,7 @@
 set fish_greeting ""
 set -gx TERM xterm-256color
-
+fish_vi_key_bindings
 . ~/.myconfig/fish/abbr.fish
-
 
 if test -f ~/.config/fish/config_local.fish
     . ~/.config/fish/config_local.fish
