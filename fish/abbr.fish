ee9a2530092844e2d14d71acbf835e7639741900
Jong-Myun Moon <jmmm@mBookAir.local>
2022-01-11 22:01:25 -0800

    exa in fish

--- fish/abbr.fish
+++ fish/abbr.fish
@@ -7,3 +7,13 @@ abbr -a ttt tree -C -L 4 -d
 abbr -a gr 'cd (git rev-parse --show-toplevel)'  # Jump to the repo root
 abbr -a gs 'git status '
 abbr -a ga 'git add '
+
+# exa
+if type -q exa
+  abbr -a ls exa
+  abbr ll exa -l -g --icons
+  abbr la exa -l -g -a --icons
+  abbr trr exa --tree --level=2 --icons
+  abbr tra exa --tree --level=3 --icons -a -l
+end
+
--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,12 @@
+set fish_greeting ""
+set -gx TERM xterm-256color
+
 . ~/.myconfig/fish/abbr.fish
+
 
 if test -f ~/.config/fish/config_local.fish
     . ~/.config/fish/config_local.fish
 end
 
 cd ~
+clear -x
